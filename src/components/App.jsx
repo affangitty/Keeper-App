98d4b7ba69af42aa0b035e72f8239fb5a772f41b
Affan Ahammed <116718291+affangitty@users.noreply.github.com>
2024-04-27 00:52:13 +0530

    Update for multiple notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import Note from './Note'
+import notes from '../notes.js'
 import "./App.css"
+const note = x=>{
+  return <Note
+  key={x.key}
+  title={x.title}
+  content={x.content}
+  />
+}
 const App = () => {
   return (
     <div>
       <Header/>
-      <Note />
+      {notes.map(note)}
       <Footer/>
     </div>
   )
